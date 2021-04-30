cd8e9d29fbe8fb16bcd5799a1aeb182a54bc06d0
Ryan Taylor <ryankt@gmail.com>
2021-04-30 01:44:25 +0000

    added Auth;

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+
+            <div class="title m-b-md">
+                Laravel
+            </div>
+
+            <example-component></example-component>
+
+            <div class="links">
+                <a href="https://laravel.com/docs">Docs</a>
+                <a href="https://laracasts.com">Laracasts</a>
+                <a href="https://laravel-news.com">News</a>
+                <a href="https://blog.laravel.com">Blog</a>
+                <a href="https://nova.laravel.com">Nova</a>
+                <a href="https://forge.laravel.com">Forge</a>
+                <a href="https://vapor.laravel.com">Vapor</a>
+                <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
