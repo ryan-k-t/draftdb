5d132f12cd0ca4036704341f21de4278cf944c30
Ryan Taylor <ryankt@gmail.com>
2025-05-14 13:07:02 +0000

    removes old links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,9 +15,8 @@
         <div class="col-2 sidebar">
             <h3 class="sidebar-title">Other Items of Interest</h3>
             <ul class="list-unstyled other-items">
-                <li><a href="https://docs.google.com/spreadsheets/d/1MrXLOukRj_7ft2OALM8np4HemcIB9IyJ40yP4bfXXcY/edit#gid=641465746" target="_blank">Mason MacRae's Draft Board</a></li>
-                <li><a href="https://www.prospectslive.com/mlb-draft/2021/4/10/450-test" target="_blank">Prospects Live</a></li>
                 <li><a href="https://www.perfectgame.org/Articles/View.aspx?article=19708&src=hmrep" target="_blank">Perfect Game "612"</a></li>
+                <li><a href="https://www.mlb.com/draft/tracker/2024" target="_blank">2024 Draft Tracker</a></li>
             </ul>
             <div class="actions"><a href="{{ url('/admin/ranking-instances/create') }}" class="btn btn-primary"><i class="fas fa-plus"></i>Add New Ranking</a></div>
         </div>
