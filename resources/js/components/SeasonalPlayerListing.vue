9d14ea1811d7323c60119b11f6ceaea839dbcc5a
Ryan Taylor <ryankt@gmail.com>
2025-05-15 21:37:42 +0000

    modifies pagination output to add descriptors

--- resources/js/components/SeasonalPlayerListing.vue
+++ resources/js/components/SeasonalPlayerListing.vue
@@ -60,9 +60,50 @@
                             </b-input-group>
                         </b-form-group>
                     </div>
+                    <div class="col">
+                        <b-form-group
+                            label="Draft Status"
+                            label-for="draft-filter"
+                            label-size="sm"
+                            class="mb-0"
+                        >
+                            <b-form-select name="draft-filter" v-model="draftStatus" :options="draftOptions" size="sm" />
+                        </b-form-group>
+                    </div>
+                    <div class="col" v-if="draftStatus === 'drafted'">
+                        <b-form-group
+                            label="Signing Status"
+                            label-for="signing-filter"
+                            label-size="sm"
+                            class="mb-0"
+                        >
+                            <b-form-select name="signing-filter" v-model="signingStatus" :options="signingOptions" size="sm" />
+                        </b-form-group>
+                    </div>
                 </div>
 
 
+                <div class="row justify-content-between align-items-center">
+                    <div class="col col-sm-2">
+                        <b-form-group
+                            label="Per page"
+                            label-for="per-page-select"
+                            label-cols-sm="4"
+                            label-size="sm"
+                            class="mb-0"
+                        >
+                            <b-form-select
+                                id="per-page-select"
+                                v-model="perPage"
+                                :options="pageOptions"
+                                size="sm"
+                            ></b-form-select>
+                        </b-form-group>
+                    </div>
+                    <div class="col text-right">
+                        {{ unfilteredRowCount.toLocaleString() }} total record{{unfilteredRowCount === 1 ? '' : 's'}}
+                    </div>
+                </div>
 
                 <b-table
                     striped
@@ -76,37 +117,26 @@
                     :filter="filter"
                     :filter-function="filterTable"
                     @filtered="onFiltered"
+                    class="mt-3 mb-5"
                 >
                     <template #cell(name)="data">
                         <a href="#" @click.prevent="displayRecord(data.item)">{{ data.item.last_name }}, {{ data.item.first_name }}</a>
                     </template>
                 </b-table>
 
-                <div class="row justify-content-between align-items-center">
-                    <div class="col-3">
-                        <b-form-group
-                            label="Per page"
-                            label-for="per-page-select"
-                            label-cols-sm="6"
-                            label-align-sm="right"
-                            label-size="sm"
-                            class="mb-0"
-                        >
-                            <b-form-select
-                                id="per-page-select"
-                                v-model="perPage"
-                                :options="pageOptions"
-                                size="sm"
-                            ></b-form-select>
-                        </b-form-group>
+                <div class="row mb-2 justify-content-between align-items-center" v-if="totalRows > 0">
+                    <div class="col text-center">
+                        Showing {{ paginationDisplaying }} of {{ totalRows.toLocaleString() }} record{{ totalRows === 1 ? '' : 's' }}
                     </div>
+                </div>
 
-                    <div class="col-8">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col">
                         <b-pagination
                             v-model="currentPage"
                             :total-rows="totalRows"
                             :per-page="perPage"
-                            align="fill"
+                            align="center"
                             class="my-0"
                         ></b-pagination>
                     </div>
@@ -226,15 +256,48 @@
                 ],
                 items: [],
                 season: null,
+                // start filters
                 classification: '',
                 position: '',
+                signingStatus: '',
+                draftStatus: '',
+                searchText: '',
+                //end filters
                 isBusy: false,
+                unfilteredRowCount: 0,
                 totalRows: 1,
                 currentPage: 1,
                 perPage: 25,
                 pageOptions: [10, 25, 50, 100, 250],
                 filter: null,
-                searchText: ''
+                draftOptions: [
+                    {
+                        value: '',
+                        text: 'All'
+                    },
+                    {
+                        value:'drafted',
+                        text: 'Drafted'
+                    },
+                    {
+                        value: 'undrafted',
+                        text: 'Undrafted'
+                    }
+                ],
+                signingOptions: [
+                    {
+                        value: '',
+                        text: 'All'
+                    },
+                    {
+                        value:'signed',
+                        text: 'Signed'
+                    },
+                    {
+                        value: 'unsigned',
+                        text: 'Unsigned'
+                    }
+                ]
             }
         },
         mounted() {
@@ -242,6 +305,7 @@
             this.season = this.currentSeason;
             // Set the initial number of items
             this.totalRows = this.items.length;
+            this.unfilteredRowCount = this.totalRows;
         },
         watch: {
             season: function(newVal, oldVal){
@@ -251,6 +315,8 @@
                 this.searchText = '';
                 this.position = '';
                 this.classification = '';
+                this.signingStatus = '';
+                this.draftStatus = '';
                 this.currentPage = 1;
             },
             searchText: function(newVal) {
@@ -259,7 +325,9 @@
                         this.filter = {
                             searchText: newVal,
                             position: '',
-                            classification: ''
+                            classification: '',
+                            signingStatus: '',
+                            draftStatus: '',
                         }
                     } else {
                         this.filter.searchText = newVal;
@@ -278,7 +346,9 @@
                         this.filter = {
                             searchText: '',
                             position: newVal,
-                            classification: ''
+                            classification: '',
+                            draftStatus: '',
+                            signingStatus: ''
                         }
                     } else {
                         this.filter.position = newVal;
@@ -297,7 +367,9 @@
                         this.filter = {
                             searchText: '',
                             position: '',
-                            classification: newVal
+                            classification: newVal,
+                            draftStatus: '',
+                            signingStatus: ''
                         }
                     } else {
                         this.filter.classification = newVal;
@@ -309,10 +381,65 @@
                 if (this.filter) {
                     this.filter.classification = '';
                 }
+            },
+            signingStatus: function(newVal) {
+                if (newVal) {
+                    if (!this.filter) {
+                        this.filter = {
+                            searchText: '',
+                            position: '',
+                            draftStatus: '',
+                            signingStatus: newVal,
+                            classification: ''
+                        }
+                    } else {
+                        this.filter.signingStatus = newVal;
+                    }
+
+                    return;
+                }
+
+                if (this.filter) {
+                    this.filter.signingStatus = '';
+                }
+            },
+            draftStatus: function(newVal) {
+                if (newVal) {
+                    if (!this.filter) {
+                        this.filter = {
+                            searchText: '',
+                            position: '',
+                            signingStatus: '',
+                            draftStatus: newVal,
+                            classification: ''
+                        }
+                    } else {
+                        this.filter.draftStatus = newVal;
+                    }
+
+                    return;
+                }
+
+                if (this.filter) {
+                    this.filter.draftStatus = '';
+                }
             }
 
         },
         computed: {
+            paginationDisplaying() {
+                if (this.totalRows === 0) {
+                    return '';
+                };
+                if (this.totalRows === 1) {
+                    return 1;
+                }
+                const low = (this.currentPage - 1) * this.perPage + 1;
+                const high = this.currentPage * this.perPage;
+                const displayHigh = Math.min(high, this.totalRows);
+
+                return `${low.toLocaleString()} of ${displayHigh.toLocaleString()}`;
+            },
             classifications() {
                 return this.initialItems.reduce((current, listItem) => {
                     if (!current.hasOwnProperty(listItem.classification)){
@@ -417,6 +544,30 @@
                     }
                 }
 
+                if (filters.draftStatus) {
+                    if (filters.draftStatus === 'drafted') {
+                        if (!row.selection) {
+                            return false;
+                        }
+
+                        if (filters.signingStatus) {
+                            if (filters.signingStatus === 'signed' && row.signed !== 1) {
+                                return false;
+                            }
+                            if (filters.signingStatus === 'unsigned' && row.signed !== 0) {
+                                return false;
+                            }
+                        }
+
+                    }
+
+                    if (filters.draftStatus === 'undrafted') {
+                        if (row.selection) {
+                            return false;
+                        }
+                    }
+                }
+
                 return true;
             },
             onFiltered(filteredItems){
@@ -439,6 +590,7 @@
                 if (this.season === this.currentSeason){
                     this.items = this.initialItems;
                     this.totalRows = this.items.length;
+                    this.unfilteredRowCount = this.totalRows;
                     this.isBusy = false;
                     return;
                 }
@@ -453,11 +605,9 @@
                             return;
                         }
 
-                        console.log(response.data);
                         this.items = response.data.records;
                         this.totalRows = this.items.length;
 
-                        //self.populateData(data);
                     } else {
                         console.log(response.status+" : "+response.statusText);
                         return;
